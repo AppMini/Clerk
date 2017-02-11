ab1658f7b0f864bd9fefe6a26136dff88709af64
Chris McCormick <chris@mccormick.cx>
2017-02-11 15:14:04 +0800

    Scripts to test API.

---
+++ bin/test-post.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 EVENT-TYPE [COMMENT...]"
+else
+  event="$1"; shift
+  if [ "$1" != "" ]
+  then
+    comment="&comment=$@"
+  fi
+
+  # https://stackoverflow.com/questions/804118/best-timestamp-format-for-csv-excel
+  timestamp=`date +"%Y-%m-%d %H:%M:%S"`
+
+  curl 'http://localhost:8000/server/' -H 'Content-Type: application/x-www-form-urlencoded' --data "event=${event}&timestamp=${timestamp}${comment}"
+fi
