995e1a4a770d1ca4cf25b5547f977a30370fcce1
Chris McCormick <chris@mccormick.cx>
2017-02-11 16:02:20 +0800

    Use full index.php path for robustness.

--- bin/test-post.sh
+++ bin/test-post.sh
@@ -13,5 +13,5 @@ else
   # https://stackoverflow.com/questions/804118/best-timestamp-format-for-csv-excel
   timestamp=`date +"%Y-%m-%d %H:%M:%S"`
 
-  curl 'http://localhost:8000/server/' -H 'Content-Type: application/x-www-form-urlencoded' --data "event=${event}&timestamp=${timestamp}${comment}"
+  curl 'http://localhost:8000/server/index.php' -H 'Content-Type: application/x-www-form-urlencoded' --data "event=${event}&timestamp=${timestamp}${comment}"
 fi
