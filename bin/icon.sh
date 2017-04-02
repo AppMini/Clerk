b6f592791d86f2d7a6acba71ccf9fbd775689a55
Chris McCormick <chris@mccormick.cx>
2017-04-02 19:27:35 +0800

    Change icon to clock.

--- bin/icon.sh
+++ bin/icon.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-font-awesome-svg-png --nopadding --sizes 192 --color "#0C9" --icons list-alt --png --dest .
-mv \#0C9/png/192/list-alt.png img/icon.png
+font-awesome-svg-png --nopadding --sizes 192 --color "#0C9" --icons clock-o --png --dest .
+mv \#0C9/png/192/clock-o.png img/icon.png
 rm -rf \#0C9
